5727f73648d452bee381c74e027a5c4ef2e64864
tavarda_mbg <daniel.t.nunes@mercedes-benz.com>
2026-04-16 16:30:47 +0100

    turn image full width

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,20 @@
 <script setup lang="ts"></script>
 
 <template>
-  <img src="./assets/home-page.png" />
+  <img src="./assets/home-page.png" class="fullscreen-image" />
 </template>
+
+<style>
+html,
+body {
+  margin: 0;
+  padding: 0;
+}
+
+.fullscreen-image {
+  width: 100vw;
+  height: 100vh;
+  object-fit: cover;
+  display: block;
+}
+</style>
